3b4f62238b57a5d310f85ae795d28dabf6f0ba18
michalantolik <michal.antolik@gmail.com>
2022-09-08 19:17:27 +0200

    Add test - open homepage

---
+++ Formy/Formy.SeleniumTests/HomepageShould.cs
@@ -0,0 +1,24 @@
+using OpenQA.Selenium.Chrome;
+
+namespace Formy.SeleniumTests
+{
+    [TestClass]
+    public class HomepageShould
+    {
+        private readonly string url = "https://formy-project.herokuapp.com/";
+
+        [TestMethod]
+        public void Open()
+        {
+            using(var driver = new ChromeDriver())
+            {
+                // ACT
+                driver.Navigate().GoToUrl(this.url);
+
+                // ASSERT
+                Assert.AreEqual(this.url, driver.Url);
+                Assert.AreEqual("Formy", driver.Title);
+            }
+        }
+    }
+}
