417270bfc40d2de80df7fb05cc15be9b5c762bf7
michalantolik <michal.antolik@gmail.com>
2022-09-09 08:07:36 +0200

    Add test - switch to window

---
+++ Formy/Formy.SeleniumTests/SwitchPage.cs
@@ -0,0 +1,34 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+
+namespace Formy.SeleniumTests
+{
+    [TestClass]
+    public class SwitchPage
+    {
+        private readonly string url = "https://formy-project.herokuapp.com/switch-window";
+
+        [TestMethod]
+        public void SwitchToWindow()
+        {
+            using(var driver = new ChromeDriver())
+            {
+                // ARRANGE
+                driver.Navigate().GoToUrl(url);
+                var newTabButton = driver.FindElement(By.Id("new-tab-button"));
+                newTabButton.Click();
+
+                // ACT
+                var originalHandle = driver.CurrentWindowHandle;
+                foreach (var handle in driver.WindowHandles)
+                {
+                    driver.SwitchTo().Window(handle);
+                }
+                driver.SwitchTo().Window(originalHandle);
+
+                // NOTHING TO ASSERT
+            }
+        }
+    }
+}
